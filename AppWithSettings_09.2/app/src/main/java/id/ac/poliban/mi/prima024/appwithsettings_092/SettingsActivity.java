3005b4c73fa5ac6b421814d4a8d2a4aaa126f5d3
primayudhi <primayudhialamsyah7@gmail.com>
2023-01-01 22:27:28 +0800

    pertemuan empatbelas

---
+++ AppWithSettings_09.2/app/src/main/java/id/ac/poliban/mi/prima024/appwithsettings_092/SettingsActivity.java
@@ -0,0 +1,18 @@
+
+
+package id.ac.poliban.mi.prima024.appwithsettings_092;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    public static final String KEY_PREF_EXAMPLE_SWITCH = "example_switch";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+}
---
+++ AppWithSettings_09.2/app/src/main/java/id/ac/poliban/mi/prima024/appwithsettings_092/SettingsFragment.java
@@ -0,0 +1,18 @@
+
+
+package id.ac.poliban.mi.prima024.appwithsettings_092;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+}
